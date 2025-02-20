e73a28c4bf63a239bfd0615f5cecdff864d009ab
Ederson Junior <ed.dev46@gmail.com>
2025-02-19 22:05:31 -0300

    Web App Template

---
+++ WebAppTemplate/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppTemplate.Models;
+
+namespace WebAppTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Other()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(User user)
+        {
+            return View("FormConfirmation", user);
+        }
+    }
+}
